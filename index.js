87f193eff002cfe1659f382683ed4c5a4ed3cff9
thebradhamter <thebradhamter@gmail.com>
2025-10-10 15:56:34 -0400

    Feature Update - 10/10/25

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const overlay = document.getElementById("previewOverlay");
 const previewImg = document.getElementById("previewImg");
 const closeBtn = document.querySelector(".close");
 const themeOpener = document.getElementById("themeOpener");
-const totalThemes = 4;
+const totalThemes = 6;
 let currentTheme;
 
 const tryCurrentTheme = Cookies.get("currentTheme");
